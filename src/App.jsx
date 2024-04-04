885d74af2e256e8e35bbe72d2f04fc49299ae089
RavinduSperera <it22064622@my.sliit.lk>
2024-04-04 13:42:21 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PlaceOrder from './pages/placeOrder'
+import ViewOrders from './pages/viewOrders'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PlaceOrder />} />
+        <Route path="/view-orders" element={<ViewOrders />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/viewOrders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViewOrders() {
+  return (
+    <div>
+        <h1>Your Orders</h1>
+    </div>
+  );
+}
